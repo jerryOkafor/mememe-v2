1cf564e90382a9f54030901ff86331c74db721c4
Jerry Hanks <jerryhanksokafor@gmail.com>
2019-07-01 18:11:16 +0100

    Initial Commit

---
+++ mememe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mememe
+//
+//  Created by Jerry Hanks on 01/07/2019.
+//  Copyright © 2019 Jerry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
